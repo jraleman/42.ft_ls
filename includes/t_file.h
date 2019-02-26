f566a0f60843aafc00dfd17957d714fcb9f30b7b
jraleman <jraleman@bendiburg.com>
2019-02-25 22:25:39 -0800

    Change prototype arg info to file

--- includes/t_file.h
+++ includes/t_file.h
@@ -26,7 +26,7 @@
 # include <stdlib.h>
 # include <string.h>
 
-# define NUM_ENTRIES		(9)
+# define NUM_ENTRIES	(9)
 
 typedef struct		s_file
 {
@@ -40,10 +40,10 @@ typedef struct		s_file
 
 void				file_swp(t_file *a, t_file *b);
 t_file				*file_ini(char *path);
-t_file				*file_del(t_file *info);
+t_file				*file_del(t_file *file);
 t_file				*file_dir(char *path, char *opt);
-t_file				*file_srt(t_file *info, char *opt);
-t_file				*file_add(t_file *info, char *name, \
+t_file				*file_srt(t_file *file, char *opt);
+t_file				*file_add(t_file *file, char *name, \
 								struct stat vstat, int type);
 
 #endif
